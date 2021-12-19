22f06bb49b055d0083793c57d88d825ddbd9edc9
SarunasL <losarunas@gmail.com>
2021-12-19 18:38:36 +0200

    added basic UI

--- script.js
+++ script.js
@@ -1,18 +1,29 @@
-let playerPlay = "";
-let roundsLeft = 5;
+let playerPlay = '';
 let playerPoints = 0;
 let computerPoints = 0;
 let roundWinner = '';
+
+let rockBtn = document.querySelector('#rockBtn');
+let scissorsBtn = document.querySelector('#scissorsBtn');
+let paperBtn = document.querySelector('#paperBtn');
+let playerScore = document.querySelector('.playerScore');
+let computerScore = document.querySelector('.computerScore');
+let selection = document.querySelector('.selection');
+let winner = document.querySelector('.winner');
+
+rockBtn.addEventListener('click', () => game('rock'))
+scissorsBtn.addEventListener('click', () => game('scissors'))
+paperBtn.addEventListener('click', () => game('paper'))
 
 function computerPlay() {
     let randomNum = Math.floor(Math.random() * 3);
     switch (randomNum) {
         case 0:
-            return "Rock";
+            return "rock";
         case 1:
-            return "Paper";
+            return "paper";
         case 2:
-            return "Scissors"
+            return "scissors"
         default:
             return "Something went wrong"
     }
@@ -23,6 +34,7 @@ function playRound(playerSelection, computerSelection) {
     let lowerCaseComputer = computerSelection.toLowerCase();
 
     if (lowerCasePlayer == lowerCaseComputer) {
+        roundWinner = "Tie"
         return "Tie you both selected: " + playerSelection;
     }
     if (lowerCasePlayer == "rock") {
@@ -52,39 +64,42 @@ function playRound(playerSelection, computerSelection) {
             return "You won you selected: " + playerSelection + " and computer selected " + computerSelection
         }
     }
-    if (!lowerCasePlayer) {
-        return "Input something"
-    }
-    if (lowerCasePlayer) {
-        return "Write without error"
-    }
 }
 
-function game() {
-    while (0 < roundsLeft) {
-        playerPlay = prompt("Choose: rock, paper or scissors:")
-        let msg = playRound(playerPlay, computerPlay());
-        if (roundWinner == "PC") {
-            console.log(msg);
-            computerPoints++;
-            roundsLeft--;
-        } else if (roundWinner == "Player") {
-            console.log(msg);
-            playerPoints++;
-            roundsLeft--;
-        } else {
-            console.log(msg);
-        }
-        console.log("Your points: " + playerPoints + " <----> PC points " + computerPoints)
-
-        if (roundsLeft == 0) {
-            if (playerPoints > computerPoints) {
-                console.log("Yeah dude you won against machine good job!")
-            } else {
-                console.log("No worries its machine its hard to win against them..")
-            }
-        }
+function game(playerSelection) {
+    let msg = playRound(playerSelection, computerPlay());
+    selection.innerHTML = msg;
+    if (roundWinner == "PC") {
+        computerPoints++;
+        computerScore.innerHTML = computerPoints;
     }
-}
+    if (roundWinner == "Player") {
+        playerPoints++;
+        playerScore.innerHTML = playerPoints;
+    }
 
-game()
+    if (computerPoints === 5) {
+        window.alert("Sadly but 'AI' won against you, don't worry it is a machine...");
+        newGame();
+    }
+    if (playerPoints === 5) {
+        window.alert("You won against machine! Damn boi");
+        newGame();
+    }
+
+    function newGame() {
+        playerPoints = 0;
+        computerPoints = 0;
+        computerScore.innerHTML = computerPoints;
+        playerScore.innerHTML = playerPoints;
+        selection.innerHTML = '';
+    }
+
+    // if (roundsLeft == 0) {
+    //     if (playerPoints > computerPoints) {
+    //         console.log("Yeah dude you won against machine good job!")
+    //     } else {
+    //         console.log("No worries its machine its hard to win against them..")
+    //     }
+    // }
+}
